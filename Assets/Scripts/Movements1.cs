a544a39e98a8f7419b93abaa2361091cdce7981e
xxxmatulisxxx <xxxmatulisxxx@gmail.com>
2016-12-06 15:43:39 +0000

    bla bla
    i done nothing yet

---
+++ Assets/Scripts/Movements1.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movements1 : MonoBehaviour {
+
+
+    float speed = 5;
+	
+	void Start () {
+	
+	}
+	
+	
+	void Update () {
+
+        if (Input.GetAxis("Horizontal") > 0) {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            transform.Translate(0, speed* Time.deltaTime, 0);
+        }
+	}
+}
